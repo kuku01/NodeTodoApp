4228ae23d8805baae4286ff91d8633ed9ded67f7
kuku_01 <harshkaranpuria@gmail.com>
2017-01-22 11:17:38 +0530

    config for heroku

--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ var {User} = require('./models/user');
 
 var app = express();
 
+const port = process.evn.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req,res) => {
@@ -51,8 +53,8 @@ app.get('/todos/:id', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('Server started @ 3000 port');
+app.listen(port, () => {
+    console.log(`Server started @ ${port} port`);
 });
 
 
