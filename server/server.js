6ae1e5fb30ad19bebbf48a07fdcc9b312398b58d
kuku_01 <harshkaranpuria@gmail.com>
2017-01-22 11:44:38 +0530

    Delete route and test case added

--- server/server.js
+++ server/server.js
@@ -25,6 +25,23 @@ app.post('/todos', (req,res) => {
     });
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
