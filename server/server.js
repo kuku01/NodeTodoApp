e71b033c18ee3a94318f28d39381d9533e50f320
kuku_01 <harshkaranpuria@gmail.com>
2017-01-26 12:11:25 +0530

    Private get route created using authentication

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const {authenticate} = require('./middleware/authenticate');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -109,6 +110,10 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
