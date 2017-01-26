f8308e364d239975fde1a3de01bba842e2f4ae2c
kuku_01 <harshkaranpuria@gmail.com>
2017-01-26 12:19:13 +0530

    user pass hashed before saving

--- Playground/hashing.js
+++ Playground/hashing.js
@@ -2,6 +2,20 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
 
+
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+var hashedPassword = '$2a$10$huAU4qTnQuGPifHEXfV9cOmPJ7p61oKaoXrY1WviiDAznE/rW8oLK';
+
+bcrypt.compare('123!', hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+
 var data = {
   id: 10
 };
