7a19db7ebafef309fca15181d8764142dff88310
kuku_01 <harshkaranpuria@gmail.com>
2017-01-26 11:37:31 +0530

    hashing password and returning auth token

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,5 @@
 /* jshintesversion:6 */
 var env = process.env.NODE_ENV || 'development';
-console.log(env);
 
 if (env === 'development') {
   process.env.PORT = 3000;
--- server/server.js
+++ server/server.js
@@ -6,42 +6,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
-const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/todo');
-const {User} = require('./models/user');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req,res) => {
-    var todo = new Todo({
-        text: req.body.text
-    });
-    todo.save().then((doc) => {
-        res.send(doc);
-    }, (error) => {
-        res.status(400).send(error);
-    });
-});
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
 
-app.delete('/todos/:id', (req, res) => {
-  var id = req.params.id;
-
-  if (!ObjectID.isValid(id)) {
-    return res.status(404).send();
-  }
-
-  Todo.findByIdAndRemove(id).then((todo) => {
-    if (!todo) {
-      return res.status(404).send();
-    }
-
-    res.send({todo});
-  }).catch((e) => {
-    res.status(400).send();
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
   });
 });
 
@@ -61,6 +43,24 @@ app.get('/todos/:id', (req, res) => {
   }
 
   Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
     if (!todo) {
       return res.status(404).send();
     }
@@ -97,9 +97,22 @@ app.patch('/todos/:id', (req, res) => {
   });
 });
 
-app.listen(port, () => {
-    console.log(`Server started @ ${port} port`);
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
 });
 
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
 
 module.exports = {app};
